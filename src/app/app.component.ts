eb349748fbf78045112ce5036db9309c3f33fe3d
André-Pascal Werthwein <andre.werthwein@gmx.de>
2024-02-25 19:33:30 +0100

    optimization of  'quality-scale'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { AppearanceLeavesModel } from './shared-data-models/appearance.model';
+import { APPEARANCE_LEAVES } from './tea-data/997-caipirinha.data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  test: AppearanceLeavesModel;
+
+  constructor() {
+    this.test = APPEARANCE_LEAVES;
+  }
+}
--- src/app/shared-components/quality-scale/quality-scale.component.ts
+++ src/app/shared-components/quality-scale/quality-scale.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { EvaluationRange } from 'src/app/shared-types/evaluation.types';
 
 @Component({
@@ -6,6 +12,26 @@ import { EvaluationRange } from 'src/app/shared-types/evaluation.types';
   templateUrl: './quality-scale.component.html',
   styleUrl: './quality-scale.component.scss',
 })
-export class QualityScaleComponent {
+export class QualityScaleComponent implements OnInit, OnChanges {
   @Input() value: EvaluationRange;
+
+  widthOverlay: number;
+
+  ngOnInit(): void {
+    this.widthOverlay = this.getPercentage();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (
+      changes['value'] &&
+      changes['value'].currentValue != changes['value'].previousValue
+    ) {
+      this.value = changes['value'].currentValue;
+      this.widthOverlay = this.getPercentage();
+    }
+  }
+
+  private getPercentage(): number {
+    return 100 - (100 / 7) * this.value;
+  }
 }
